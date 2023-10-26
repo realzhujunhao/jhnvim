a355094433867587f6fa0bb5def7d4cf2f64f790
Junhao Zhu <63294481+realzhujunhao@users.noreply.github.com>
2023-10-27 00:32:14 +0900

    Update plugins-setup.lua

--- nvim/lua/junhaozhu/plugins-setup.lua
+++ nvim/lua/junhaozhu/plugins-setup.lua
@@ -45,7 +45,6 @@ return packer.startup(function(use)
 	use("nvim-tree/nvim-tree.lua") -- file explorer
 	use("kyazdani42/nvim-web-devicons") -- icons
 	use("nvim-lualine/lualine.nvim") --statusline
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
 	-- configure lsp
